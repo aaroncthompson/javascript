43f7ac40df4f034f354dbda5ecff66c8017ff21f
Aaron Thompson <acthompson.w@gmail.com>
2016-02-29 15:12:34 -0800

    Create meh..js
    I sure hope I'm allowed to just reuse "number" like that.

---
+++ meh..js
@@ -0,0 +1,32 @@
+/* Initial variables */
+var happiness = 25; // baseline 50
+var health = 30; // baseline 50
+var knowledge = 10; // baseline 50
+var day = 1;
+var money = 500;
+var career = 0; // baseline 50
+
+/* Global functions */
+var addhappy = function (number) {
+  happiness += number;
+};
+
+var addhealth = function (number) {
+  health += number;
+};
+
+var addknowledge = function (number) {
+  knowledge += number;
+};
+
+var addday = function (number) {
+  day += number;
+};
+
+var addmoney = function (number) {
+  money += number;
+};
+
+var addcareer = function (career) {
+  career += number;
+};
